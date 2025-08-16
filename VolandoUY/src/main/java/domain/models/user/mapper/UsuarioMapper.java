7281b4a31d18ce359af84e44ed6b21d6f1cd9e78
Nacho <102491623+dottox@users.noreply.github.com>
2025-08-16 20:50:08 -0300

    fix: aerolinea typo

--- VolandoUY/src/main/java/domain/models/user/mapper/UsuarioMapper.java
+++ VolandoUY/src/main/java/domain/models/user/mapper/UsuarioMapper.java
@@ -1,9 +1,9 @@
 package domain.models.user.mapper;
 
-import domain.dtos.user.AereolineaDTO;
+import domain.dtos.user.AerolineaDTO;
 import domain.dtos.user.ClienteDTO;
 import domain.dtos.user.UsuarioDTO;
-import domain.models.user.Aereolinea;
+import domain.models.user.Aerolinea;
 import domain.models.user.Cliente;
 import domain.models.user.Usuario;
 import org.modelmapper.ModelMapper;
@@ -19,7 +19,7 @@ public class UsuarioMapper {
         if (usuario instanceof Cliente) {
             return modelMapper.map(usuario, ClienteDTO.class);
         } else if (usuario instanceof Aereolinea) {
-            return modelMapper.map(usuario, AereolineaDTO.class);
+            return modelMapper.map(usuario, AerolineaDTO.class);
         }
         throw new IllegalArgumentException("Tipo de usuario desconocido: " + usuario.getClass());
     }
