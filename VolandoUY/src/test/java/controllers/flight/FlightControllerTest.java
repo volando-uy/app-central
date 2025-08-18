a58134f65844ed810098e92d8f9eec383575d61b
dottox <imsm2424@gmail.com>
2025-08-18 19:22:27 -0300

    feat: tests for flight controller and service

---
+++ VolandoUY/src/test/java/controllers/flight/FlightControllerTest.java
@@ -0,0 +1,55 @@
+package controllers.flight;
+
+import controllers.flightRoute.FlightRouteController;
+import controllers.flightRoute.IFlightRouteController;
+import domain.dtos.flight.FlightDTO;
+import domain.dtos.flightRoute.CategoryDTO;
+import domain.services.flight.IFlightService;
+import domain.services.flightRoute.IFlightRouteService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.modelmapper.ModelMapper;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+public class FlightControllerTest {
+
+    @Mock
+    private IFlightService flightService;
+
+    @InjectMocks
+    private IFlightController flightController;
+
+    @BeforeEach
+    void setUp() {
+        flightService = mock(IFlightService.class);
+        flightController = new FlightController(flightService);
+    }
+
+    @Test
+    @DisplayName("Debe llamar a createFlight correctamente")
+    void createFlight_shouldCallTheServiceCorrectly() {
+        FlightDTO flightDTO = new FlightDTO();
+        flightDTO.setName("test");
+
+        // Act
+        flightController.createFlight(flightDTO);
+
+        // Assert
+        verify(flightService).createFlight(flightDTO);
+    }
+
+    @Test
+    @DisplayName("Debe llamar a getAllFlights correctamente")
+    void getAllFlights_shouldCallTheServiceCorrectly() {
+        // Act
+        flightController.getAllFlights();
+
+        // Assert
+        verify(flightService).getAllFlights();
+    }
+}
