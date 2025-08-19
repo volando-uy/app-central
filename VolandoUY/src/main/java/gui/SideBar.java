ab35a804d3b0c5ae5294bb56ee73dc81fd08b62c
dottox <imsm2424@gmail.com>
2025-08-19 01:42:20 -0300

    refactor: now the gui is separated in components

---
+++ VolandoUY/src/main/java/gui/SideBar.java
@@ -0,0 +1,134 @@
+/*
+ * Created by JFormDesigner on Mon Aug 18 23:06:29 UYT 2025
+ */
+
+package gui;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.GroupLayout;
+
+import lombok.Setter;
+
+/**
+ * @author ignac
+ */
+public class SideBar extends JPanel {
+
+    private MouseListener userButtonListener;
+
+    public SideBar(MouseListener userListener) {
+        userButtonListener = userListener;
+        initComponents();
+        initListeners();
+    }
+
+    private void initListeners() {
+        UserButton.addMouseListener(userButtonListener);
+    }
+
+    private void initComponents() {
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents  @formatter:off
+        // Generated using JFormDesigner Evaluation license - dotto
+        Icon = new JLabel();
+        MenuPanel = new JPanel();
+        UserButton = new JButton();
+        FlightManagementButton = new JButton();
+        PackageManagementButton = new JButton();
+        AirlineManagementButton = new JButton();
+        ReservationsButton = new JButton();
+
+        //======== this ========
+        setBackground(new Color(0x14213d));
+        setPreferredSize(new Dimension(160, 600));
+        setMinimumSize(new Dimension(80, 300));
+        setMaximumSize(new Dimension(160, 600));
+        setBorder ( new javax . swing. border .CompoundBorder ( new javax . swing. border .TitledBorder ( new javax . swing
+        . border .EmptyBorder ( 0, 0 ,0 , 0) ,  "JF\u006frm\u0044es\u0069gn\u0065r \u0045va\u006cua\u0074io\u006e" , javax. swing .border . TitledBorder
+        . CENTER ,javax . swing. border .TitledBorder . BOTTOM, new java. awt .Font ( "D\u0069al\u006fg", java .
+        awt . Font. BOLD ,12 ) ,java . awt. Color .red ) , getBorder () ) )
+        ;  addPropertyChangeListener( new java. beans .PropertyChangeListener ( ){ @Override public void propertyChange (java . beans. PropertyChangeEvent e
+        ) { if( "\u0062or\u0064er" .equals ( e. getPropertyName () ) )throw new RuntimeException( ) ;} } )
+        ;
+        setLayout(new BorderLayout());
+
+        //---- Icon ----
+        Icon.setIcon(new ImageIcon(getClass().getResource("/volando_uy_logo_resized.png")));
+        add(Icon, BorderLayout.PAGE_START);
+
+        //======== MenuPanel ========
+        {
+            MenuPanel.setBackground(new Color(0x14213d));
+            MenuPanel.setMaximumSize(new Dimension(160, 470));
+            MenuPanel.setMinimumSize(new Dimension(160, 470));
+
+            //---- UserButton ----
+            UserButton.setText("Usuario");
+            UserButton.setForeground(new Color(0x3b82f6));
+            UserButton.setFont(new Font("Leelawadee UI", Font.BOLD, 13));
+
+            //---- FlightManagementButton ----
+            FlightManagementButton.setText("Gesti\u00f3n de Vuelos");
+            FlightManagementButton.setFont(new Font("Leelawadee UI", Font.BOLD, 13));
+            FlightManagementButton.setForeground(new Color(0x3b82f6));
+
+            //---- PackageManagementButton ----
+            PackageManagementButton.setText("Gesti\u00f3n de Paquetes");
+            PackageManagementButton.setFont(new Font("Leelawadee UI", Font.BOLD, 13));
+            PackageManagementButton.setForeground(new Color(0x3b82f6));
+
+            //---- AirlineManagementButton ----
+            AirlineManagementButton.setText("Gesti\u00f3n de Aerol\u00ednea");
+            AirlineManagementButton.setFont(new Font("Leelawadee UI", Font.BOLD, 13));
+            AirlineManagementButton.setForeground(new Color(0x3b82f6));
+
+            //---- ReservationsButton ----
+            ReservationsButton.setText("Reservas");
+            ReservationsButton.setFont(new Font("Leelawadee UI", Font.BOLD, 13));
+            ReservationsButton.setForeground(new Color(0x3b82f6));
+
+            GroupLayout MenuPanelLayout = new GroupLayout(MenuPanel);
+            MenuPanel.setLayout(MenuPanelLayout);
+            MenuPanelLayout.setHorizontalGroup(
+                MenuPanelLayout.createParallelGroup()
+                    .addGroup(MenuPanelLayout.createSequentialGroup()
+                        .addGroup(MenuPanelLayout.createParallelGroup()
+                            .addComponent(PackageManagementButton)
+                            .addComponent(FlightManagementButton, GroupLayout.PREFERRED_SIZE, 160, GroupLayout.PREFERRED_SIZE)
+                            .addComponent(UserButton, GroupLayout.PREFERRED_SIZE, 160, GroupLayout.PREFERRED_SIZE)
+                            .addComponent(AirlineManagementButton, GroupLayout.PREFERRED_SIZE, 160, GroupLayout.PREFERRED_SIZE)
+                            .addComponent(ReservationsButton, GroupLayout.PREFERRED_SIZE, 160, GroupLayout.PREFERRED_SIZE))
+                        .addGap(0, 0, Short.MAX_VALUE))
+            );
+            MenuPanelLayout.setVerticalGroup(
+                MenuPanelLayout.createParallelGroup()
+                    .addGroup(MenuPanelLayout.createSequentialGroup()
+                        .addGap(30, 30, 30)
+                        .addComponent(UserButton)
+                        .addGap(30, 30, 30)
+                        .addComponent(FlightManagementButton)
+                        .addGap(30, 30, 30)
+                        .addComponent(PackageManagementButton)
+                        .addGap(30, 30, 30)
+                        .addComponent(AirlineManagementButton)
+                        .addGap(30, 30, 30)
+                        .addComponent(ReservationsButton)
+                        .addContainerGap(150, Short.MAX_VALUE))
+            );
+        }
+        add(MenuPanel, BorderLayout.CENTER);
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents  @formatter:on
+    }
+
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables  @formatter:off
+    // Generated using JFormDesigner Evaluation license - dotto
+    private JLabel Icon;
+    private JPanel MenuPanel;
+    private JButton UserButton;
+    private JButton FlightManagementButton;
+    private JButton PackageManagementButton;
+    private JButton AirlineManagementButton;
+    private JButton ReservationsButton;
+    // JFormDesigner - End of variables declaration  //GEN-END:variables  @formatter:on
+}
--- VolandoUY/src/main/java/gui/user/UserFrame.java
+++ VolandoUY/src/main/java/gui/user/UserFrame.java
@@ -2,6 +2,7 @@ package gui.user;
 
 import javax.swing.*;
 import java.awt.*;
+import com.jgoodies.forms.layout.*;
 
 public class UserFrame extends JPanel {
     public UserFrame() {
@@ -9,25 +10,89 @@ public class UserFrame extends JPanel {
     }
 
     // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables  @formatter:off
-    // Generated using JFormDesigner Evaluation license - Nahuel
-    private JLabel label1;
+    // Generated using JFormDesigner Evaluation license - dotto
+    private JPanel NavPanel;
     private JButton button1;
+    private JButton button2;
+    private JPanel ContentPanel;
     // JFormDesigner - End of variables declaration  //GEN-END:variables  @formatter:on
 
     private void initComponents() {
         // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents  @formatter:off
-        // Generated using JFormDesigner Evaluation license - Nahuel
-        label1 = new JLabel();
+        // Generated using JFormDesigner Evaluation license - dotto
+        NavPanel = new JPanel();
         button1 = new JButton();
+        button2 = new JButton();
+        ContentPanel = new JPanel();
 
-        //---- label1 ----
-        label1.setText("text");
+        //======== this ========
+        setPreferredSize(new Dimension(640, 600));
+        setMinimumSize(new Dimension(320, 300));
+        setMaximumSize(new Dimension(640, 600));
+        setBackground(new Color(0x2a406c));
+        setBorder ( new javax . swing. border .CompoundBorder ( new javax . swing. border .TitledBorder ( new javax . swing. border
+        .EmptyBorder ( 0, 0 ,0 , 0) ,  "JF\u006frmD\u0065sig\u006eer \u0045val\u0075ati\u006fn" , javax. swing .border . TitledBorder. CENTER ,javax
+        . swing. border .TitledBorder . BOTTOM, new java. awt .Font ( "Dia\u006cog", java .awt . Font. BOLD ,
+        12 ) ,java . awt. Color .red ) , getBorder () ) );  addPropertyChangeListener( new java. beans
+        .PropertyChangeListener ( ){ @Override public void propertyChange (java . beans. PropertyChangeEvent e) { if( "\u0062ord\u0065r" .equals ( e.
+        getPropertyName () ) )throw new RuntimeException( ) ;} } );
+        setLayout(new BorderLayout());
 
-        //---- button1 ----
-        button1.setText("text");
+        //======== NavPanel ========
+        {
+            NavPanel.setMinimumSize(new Dimension(200, 30));
+            NavPanel.setPreferredSize(new Dimension(640, 60));
+            NavPanel.setMaximumSize(new Dimension(640, 30));
+            NavPanel.setBackground(new Color(0x333333));
+
+            //---- button1 ----
+            button1.setText("text");
+
+            //---- button2 ----
+            button2.setText("text");
+
+            GroupLayout NavPanelLayout = new GroupLayout(NavPanel);
+            NavPanel.setLayout(NavPanelLayout);
+            NavPanelLayout.setHorizontalGroup(
+                NavPanelLayout.createParallelGroup()
+                    .addGroup(NavPanelLayout.createSequentialGroup()
+                        .addGap(42, 42, 42)
+                        .addComponent(button1, GroupLayout.PREFERRED_SIZE, 130, GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(button2, GroupLayout.PREFERRED_SIZE, 130, GroupLayout.PREFERRED_SIZE)
+                        .addContainerGap(320, Short.MAX_VALUE))
+            );
+            NavPanelLayout.setVerticalGroup(
+                NavPanelLayout.createParallelGroup()
+                    .addGroup(GroupLayout.Alignment.TRAILING, NavPanelLayout.createSequentialGroup()
+                        .addContainerGap(20, Short.MAX_VALUE)
+                        .addGroup(NavPanelLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                            .addComponent(button1)
+                            .addComponent(button2))
+                        .addContainerGap())
+            );
+        }
+        add(NavPanel, BorderLayout.NORTH);
+
+        //======== ContentPanel ========
+        {
+            ContentPanel.setPreferredSize(new Dimension(200, 200));
+            ContentPanel.setMinimumSize(new Dimension(200, 200));
+            ContentPanel.setBackground(new Color(0x2a406c));
+
+            GroupLayout ContentPanelLayout = new GroupLayout(ContentPanel);
+            ContentPanel.setLayout(ContentPanelLayout);
+            ContentPanelLayout.setHorizontalGroup(
+                ContentPanelLayout.createParallelGroup()
+                    .addGap(0, 640, Short.MAX_VALUE)
+            );
+            ContentPanelLayout.setVerticalGroup(
+                ContentPanelLayout.createParallelGroup()
+                    .addGap(0, 0, Short.MAX_VALUE)
+            );
+        }
+        add(ContentPanel, BorderLayout.CENTER);
         // JFormDesigner - End of component initialization  //GEN-END:initComponents  @formatter:on
-        add(label1);
-        add(button1);
     }
 }
 
