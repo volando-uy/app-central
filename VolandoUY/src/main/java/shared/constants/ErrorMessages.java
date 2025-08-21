2ffcc969576e8e9750ad926a084fa69d256c3bc8
nahuelutec <nahuel.martinez.a@estudiantes.utec.edu.uy>
2025-08-21 17:28:13 -0300

    fix: web field GUI and fixed error message when creating a user

--- VolandoUY/src/main/java/shared/constants/ErrorMessages.java
+++ VolandoUY/src/main/java/shared/constants/ErrorMessages.java
@@ -2,7 +2,7 @@ package shared.constants;
 
 public class ErrorMessages {
     public static final String ERR_CAMPO_OBLIGATORIO="El campo %s es obligatorio";
-    public static final String ERR_USUARIO_YA_EXISTE="El usuario %s ya existe";
+    public static final String ERR_USUARIO_YA_EXISTE="El nickname o email ya están en uso";
     public static final String ERR_CLIENTE_NO_EXISTE_CLIENTE="El cliente %s no existe";
     public static final String ERR_FORMATO_EMAIL_INVALIDO="Formato de email invalido";
     public static final String ERR_ACCEDER_CAMPO = "Error al acceder al campo %s";
