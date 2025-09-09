365fd67ac37cbb31747878f6de39b9dc1dc99f3d
nahuelutec <nahuel.martinez.a@estudiantes.utec.edu.uy>
2025-09-09 17:23:25 -0300

    agregando metodo a flightrouterepo

--- VolandoUY/src/main/java/infra/repository/booking/IBookingRepository.java
+++ VolandoUY/src/main/java/infra/repository/booking/IBookingRepository.java
@@ -1,6 +1,7 @@
 package infra.repository.booking;
 
 import domain.models.bookflight.BookFlight;
+import domain.models.buypackage.BuyPackage;
 import domain.models.seat.Seat;
 import domain.models.ticket.Ticket;
 import domain.models.user.Customer;
@@ -9,5 +10,5 @@ import java.util.List;
 
 public interface IBookingRepository {
     void saveBookflightWithTicketsAndAddToSeats(BookFlight bookFlight, List<Ticket> savedTickets, List<Seat> seats, Customer customer);
-
+    List<BookFlight> findByCustomerNickname(String nickname);
 }
