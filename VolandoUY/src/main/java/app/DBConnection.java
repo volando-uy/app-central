aa78cdc475e4569af546ae7158d572720849a0d3
gyabisito <josecitohernandez5@gmail.com>
2025-11-27 23:36:13 -0300

    ahora la bd se conecta a travez de applciation.properties

--- VolandoUY/src/main/java/app/DBConnection.java
+++ VolandoUY/src/main/java/app/DBConnection.java
@@ -1,66 +1,31 @@
 package app;
 
+import app.config.JPAUtil;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
-import jakarta.persistence.Persistence;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class DBConnection {
-
-    private static final String ENVIRONMENT = System.getenv("ENVIRONMENT");
 
     private static EntityManagerFactory emf = null;
 
     public static EntityManagerFactory getEntityManagerFactory() {
-
-        String persistenceUnitName = ENVIRONMENT != null && ENVIRONMENT.equals("PROD") ? "VolandoAppProd" : "VolandoApp";
-
-        if (persistenceUnitName.equals("VolandoAppProd")) {
-            String dbUrl = System.getenv("DATABASE_URL");
-
-            Pattern pattern = Pattern.compile("postgres://([^:]+):([^@]+)@([^:/]+):(\\d+)/(\\w+)");
-            Matcher matcher = pattern.matcher(dbUrl);
-
-            if (!matcher.matches()) {
-                throw new IllegalArgumentException("DATABASE_URL format is invalid");
-            }
-
-            String dbUser = matcher.group(1);
-            String dbPassword = matcher.group(2);
-            String dbHost = matcher.group(3);
-            String dbPort = matcher.group(4);
-            String dbName = matcher.group(5);
-
-            Map<String, String> properties = new HashMap<>();
-            properties.put("jakarta.persistence.jdbc.url", String.format("jdbc:postgresql://%s:%s/%s", dbHost, dbPort, dbName));
-            properties.put("jakarta.persistence.jdbc.user", dbUser);
-            properties.put("jakarta.persistence.jdbc.password", dbPassword);
-            properties.put("jakarta.persistence.jdbc.driver", "org.postgresql.Driver");
-            return Persistence.createEntityManagerFactory(persistenceUnitName, properties);
-        } else {
-            return Persistence.createEntityManagerFactory(persistenceUnitName);
+        if (emf == null) {
+            emf = JPAUtil.getEntityManagerFactory();
         }
+        return emf;
     }
 
     public static EntityManager getEntityManager() {
-        if (emf == null) {
-            emf = getEntityManagerFactory();
-        }
-        return emf.createEntityManager();
+        return getEntityManagerFactory().createEntityManager();
     }
 
     public static void cleanDB() {
-        EntityManager em = DBConnection.getEntityManager();
+        EntityManager em = getEntityManager();
         EntityTransaction tx = em.getTransaction();
+
         try {
             tx.begin();
-
-
 
             em.createNativeQuery("TRUNCATE TABLE airline CASCADE").executeUpdate();
             em.createNativeQuery("TRUNCATE TABLE airport CASCADE").executeUpdate();
@@ -80,16 +45,13 @@ public class DBConnection {
             em.createNativeQuery("TRUNCATE TABLE ticket CASCADE").executeUpdate();
             em.createNativeQuery("TRUNCATE TABLE follow CASCADE").executeUpdate();
 
-            tx.commit(); // ✅ solo si todo fue bien
+            tx.commit();
+
         } catch (Exception e) {
-            if (tx.isActive()) {
-                tx.rollback(); // ❌ si falló, deshacemos
-            }
+            if (tx.isActive()) tx.rollback();
             throw new RuntimeException("Error al limpiar la base de datos", e);
         } finally {
-            if (em.isOpen()) {
-                em.close(); // 🔒 siempre cerramos el EM
-            }
+            if (em.isOpen()) em.close();
         }
     }
 }
--- VolandoUY/src/main/java/app/VolandoApp.java
+++ VolandoUY/src/main/java/app/VolandoApp.java
@@ -4,8 +4,10 @@ import app.adapters.soap.SoapServicePublisher;
 import app.adapters.soap.airport.AirportSoapAdapter;
 import app.config.ConfigProperties;
 import app.config.DBInitThread;
+import app.config.JPAUtil;
 import factory.ControllerFactory;
 import gui.MainFrame;
+import jakarta.persistence.EntityManagerFactory;
 import org.apache.cxf.Bus;
 import org.apache.cxf.BusFactory;
 import org.apache.cxf.jaxws.EndpointImpl;
@@ -17,6 +19,12 @@ public class VolandoApp {
 
 
     public static void main(String[] args) {
+        System.out.println("SOAP URL: " + ConfigProperties.get("soap.ip") + ":" +
+                ConfigProperties.getInt("soap.port") +
+                ConfigProperties.get("soap.path"));
+
+        // Inicializar DB con JPA
+        EntityManagerFactory emf = JPAUtil.getEntityManagerFactory();
 
         new DBInitThread().start();
 
